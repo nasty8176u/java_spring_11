73fdffe7cab81860b163ec93f21e273ddb490c74
nasty8176u <110005925+nasty8176u@users.noreply.github.com>
2024-02-25 23:02:52 +0900

    Add files via upload

---
+++ WebPageMicroService/src/main/java/ru/fsv67/WebPageMicroService.java
@@ -0,0 +1,11 @@
+package ru.fsv67;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPageMicroService {
+    public static void main(String[] args) {
+        SpringApplication.run(WebPageMicroService.class, args);
+    }
+}
