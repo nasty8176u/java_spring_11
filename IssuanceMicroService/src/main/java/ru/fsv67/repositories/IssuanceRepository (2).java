c3267ed9317dda8a983bc0142458e696da15842b
nasty8176u <110005925+nasty8176u@users.noreply.github.com>
2024-02-25 22:14:00 +0900

    Add files via upload

---
+++ IssuanceMicroService/src/main/java/ru/fsv67/repositories/IssuanceRepository (2).java
@@ -0,0 +1,23 @@
+package ru.fsv67.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.fsv67.Issuance;
+
+import java.util.List;
+
+/**
+ * Класс описывает выдачу книг читателям
+ */
+@Repository
+public interface IssuanceRepository extends JpaRepository<Issuance, Long> {
+    /**
+     * Метод найти все выдачи по идентификатору читателя
+     *
+     * @param id идентификатор читателя
+     * @return список выданных книг
+     */
+    @Query("SELECT I FROM Issuance I WHERE I.readerId = :id and I.returned_at IS NULL")
+    List<Issuance> findIssuanceByReaderId(long id);
+}
